aa753e99b32ce0be1780e54d3399e5afeedeb18a
PseudoNIMB <ienndevld@yandex.ru>
2024-01-31 01:13:59 +0300

    Nothing to report

--- app/src/main/java/ru/pseudonimb/onyxcalc/HomeFragment.kt
+++ app/src/main/java/ru/pseudonimb/onyxcalc/HomeFragment.kt
@@ -9,13 +9,20 @@ import ru.pseudonimb.onyxcalc.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnOne.setOnClickListener{
+            binding.btnTwo.setBackgroundResource(R.mipmap.ic_launcher)
+        }
+    }
 }
