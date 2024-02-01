31f522ae2218336641147429be9fed17b2f51fa8
a.tatarevskiy <tatarevskijaa@mial.ru>
2024-02-01 13:29:02 +0300

    Так работает:)

--- app/src/main/java/ru/pseudonimb/onyxcalc/HomeFragment.kt
+++ app/src/main/java/ru/pseudonimb/onyxcalc/HomeFragment.kt
@@ -1,21 +1,32 @@
 package ru.pseudonimb.onyxcalc
 
-import android.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+import androidx.fragment.app.Fragment
 import ru.pseudonimb.onyxcalc.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("my_log", "loaded")
+        binding.C.setOnClickListener {
+            Log.d("my_log", "btn pressed")
+        }
     }
 
 }
--- app/src/main/java/ru/pseudonimb/onyxcalc/MainActivity.kt
+++ app/src/main/java/ru/pseudonimb/onyxcalc/MainActivity.kt
@@ -1,23 +1,16 @@
 package ru.pseudonimb.onyxcalc
-
-import android.app.Activity
 import android.os.Bundle
-import ru.pseudonimb.onyxcalc.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
-class MainActivity : Activity() {
-    private lateinit var binding: ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        fragmentManager
-            .beginTransaction()
-            .add(R.id.fragment_home_container, HomeFragment())
-            .addToBackStack(null)
-            .commit()
-    }
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_home_container, HomeFragment())
+            .commitAllowingStateLoss()
+        }
 }
